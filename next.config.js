36d61f4e9551e3393bf0d1ec79e93f7066e61605
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2023-09-06 11:16:33 +0300

    deployment

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    basePath: '/NEXT-events-application', // Replace with your repository name
+    assetPrefix: '/NEXT-events-application/', // Replace with your repository name
+  };
